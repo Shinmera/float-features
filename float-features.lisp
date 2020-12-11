88f2adb98b21516b35ca67653767cc2574bc4951
Masataro Asai <guicho2.71828@gmail.com>
2020-12-11 01:16:09 -0500

    XX-float-NaN init

--- float-features.lisp
+++ float-features.lisp
@@ -10,12 +10,16 @@
   (:export
    #:short-float-positive-infinity
    #:short-float-negative-infinity
+   #:short-float-nan
    #:single-float-positive-infinity
    #:single-float-negative-infinity
+   #:single-float-nan
    #:double-float-positive-infinity
    #:double-float-negative-infinity
+   #:double-float-nan
    #:long-float-positive-infinity
    #:long-float-negative-infinity
+   #:long-float-nan
    #:float-infinity-p
    #:float-nan-p
    #:with-float-traps-masked
